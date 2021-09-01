06b5fd104b90eaa4668ee576cf3e022a14d8d1c8
sosma <osma.ovaskainen@helsinki.fi>
2021-09-01 14:48:24 +0300

    Update tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -43,44 +43,5 @@ async def main():
                     for user in messages.action.users:
                         msg = await client.kick_participant(my_chat, user)
                         await msg.delete()
-            #with open("authorized.txt", "a") as f:
-                #f.write(str(await client.is_user_authorized(users)) + "\n")
-
-    # You can send messages to yourself...
-    #await client.send_message('me', 'Hello, myself!')
-    # ...to some chat ID
-    #await client.send_message(-100123456, 'Hello, group!')
-    # ...to your contacts
-    #await client.send_message('+34600123123', 'Hello, friend!')
-    # ...or even to any username
-    #await client.send_message('username', 'Testing Telethon!')
-
-    # You can, of course, use markdown in your messages:
-    #message = await client.send_message(
-    #    'me',
-    #    'This message has **bold**, `code`, __italics__ and '
-    #    'a [nice website](https://example.com)!',
-    #    link_preview=False
-    #)
-
-    # Sending a message returns the sent message object, which you can use
-    #print(message.raw_text)
-
-    # You can reply to messages directly if you have a message object
-    #await message.reply('Cool!')
-
-    # Or send files, songs, documents, albums...
-    #await client.send_file('me', '/home/me/Pictures/holidays.jpg')
-
-    # You can print the message history of any chat:
-    #async for message in client.iter_messages('me'):
-    #    print(message.id, message.text)
-
-        # You can download media from messages, too!
-        # The method will return the path where the file was saved.
-    #    if message.photo:
-    #        path = await message.download_media()
-    #        print('File saved to', path)  # printed after download is done
-
 with client:
     client.loop.run_until_complete(main())
